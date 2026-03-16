ebb19c34a97b1cf65ec3785af014027eb76feb81
Sandeep Reddy Modugu <104361695+7997275647@users.noreply.github.com>
2026-03-16 17:42:11 +0100

    Add base path for EmbeddedC_App
    Set the base path for the application.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/EmbeddedC_App/', // Add this line! Use your repo name.
   plugins: [react()],
 })
